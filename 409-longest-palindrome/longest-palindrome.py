3c3ad7dd80ec5f9934001bd7cf58137f0a7e7350
Tonatiuh Leung-Arellano <tonatiuhula8@gmail.com>
2026-08-15 09:33:23 -0400

    Time: 3 ms (45.97%) | Memory: 19.2 MB (67.30%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        chars = {}
+        ret = 0
+
+        for ch in s:
+            chars[ch] = chars[ch] + 1 if ch in chars else 1
+        
+        truke = False
+        for key, val in chars.items():
+            if val % 2 != 0:
+                if truke:
+                    ret += val - 1
+                else:
+                    ret += val
+                    truke = True
+            else:
+                ret += val
+        '''
+        if truke and ret % 2 == 0:
+            print("adding one")
+            ret += 1 '''
+        return ret
